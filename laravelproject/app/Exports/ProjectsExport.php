cc9ab854caae37f3d3abba62085504819bf44db0
charbelkiwan <charbel_kiwan@outlook.com>
2023-08-23 13:46:28 +0300

    change ProjectsExport

--- laravelproject/app/Exports/ProjectsExport.php
+++ laravelproject/app/Exports/ProjectsExport.php
@@ -15,10 +15,7 @@ class ProjectsExport implements WithHeadings, FromQuery, WithMapping
 
     public function query()
     {
-        return Project::select('id', 'title', 'description')
-            ->with(['tasks' => function ($query) {
-                $query->select('title');
-            }]);
+        return Project::query()->with('tasks:id,title');
     }
 
     public function headings(): array
@@ -26,16 +23,17 @@ class ProjectsExport implements WithHeadings, FromQuery, WithMapping
         return [
             'Project Name',
             'Task Name',
+            'Project Due Date',
         ];
     }
 
     public function map($project): array
     {
         $project->load('tasks');
-
         return [
             $project->title,
             implode(" | ", $project->tasks->pluck('title')->toArray()),
+            $project->due_date,
         ];
     }
 }
